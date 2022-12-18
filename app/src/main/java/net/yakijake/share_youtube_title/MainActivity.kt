18b5fb8020b4fe0c71f1dec1f04f12e3a5e4665a
yakijake <yakisugita@gmail.com>
2022-12-18 21:41:19 +0900

    ToolBar作成.設定&共有アクティビティ追加.okhttp3導入

--- app/src/main/java/net/yakijake/share_youtube_title/MainActivity.kt
+++ app/src/main/java/net/yakijake/share_youtube_title/MainActivity.kt
@@ -1,11 +1,47 @@
 package net.yakijake.share_youtube_title
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // activity_toolbar_sample.xml からToolbar要素を取得
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // アクションバーにツールバーをセット
+        setSupportActionBar(toolbar)
+        // ツールバーに戻るボタンを設置
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // ActionBarのボタンが押されたとき
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            Toast.makeText(applicationContext, "アプリ設定を開きます", Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, SettingsActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        android.R.id.home -> {
+            // Activity終了
+            finish()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    // 右上にボタンを表示
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
---
+++ app/src/main/java/net/yakijake/share_youtube_title/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package net.yakijake.share_youtube_title
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        // activity_toolbar_sample.xml からToolbar要素を取得
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // アクションバーにツールバーをセット
+        setSupportActionBar(toolbar)
+        // ツールバーに戻るボタンを設置
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+}
